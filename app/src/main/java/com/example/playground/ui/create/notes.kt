7cf117a224db52bbc17edf05ef2265f358126f00
KMalif <khafidulmualif2019@gmail.com>
2020-12-10 02:23:44 +0700

    add activity notes

---
+++ app/src/main/java/com/example/playground/ui/create/notes.kt
@@ -0,0 +1,12 @@
+package com.example.playground.ui.create
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.playground.R
+
+class notes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes)
+    }
+}
